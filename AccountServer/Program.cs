56a253e1e7e43711efd27a55c1378649efa5d091
Adam-WY-Jeong <jwy0805@gmail.com>
2024-04-29 10:08:11 +0900

    Swagger has deprecated from web server Program.cs

--- AccountServer/Program.cs
+++ AccountServer/Program.cs
@@ -32,20 +32,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // -- StartUp.cs - Configure
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger(); 
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
