1963c140b928bcba1d181add40e77c75e219fbda
Adam-WY-Jeong <jwy0805@gmail.com>
2024-05-02 14:10:11 +0900

    0502 test

--- AccountServer/Program.cs
+++ AccountServer/Program.cs
@@ -6,15 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ConfigService>();
 
-var configService = new ConfigService();
-var path = Environment.GetEnvironmentVariable("CONFIG_PATH") ??
-           "/Users/jwy/Documents/Dev/CryWolf/Config/CryWolfAccountConfig.json";
-var appConfig = configService.LoadGoogleConfigs(path);
-
-// Add services to the container.
-// -- StartUp.cs
-var defaultConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
-                              builder.Configuration.GetConnectionString("DefaultConnection");
+// var configService = new ConfigService();
+// var path = Environment.GetEnvironmentVariable("CONFIG_PATH") ??
+//            "/Users/jwy/Documents/Dev/CryWolf/Config/CryWolfAccountConfig.json";
+// var appConfig = configService.LoadGoogleConfigs(path);
+//
+// // Add services to the container.
+// // -- StartUp.cs
+// var defaultConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
+//                               builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
@@ -22,38 +22,38 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.DictionaryKeyPolicy = null;
 });
 
-builder.Services.AddAuthentication()
-    .AddGoogle(options =>
-    {
-        options.ClientId = appConfig.GoogleClientId;
-        options.ClientSecret = appConfig.GoogleClientSecret;
-    });
+// builder.Services.AddAuthentication()
+//     .AddGoogle(options =>
+//     {
+//         options.ClientId = appConfig.GoogleClientId;
+//         options.ClientSecret = appConfig.GoogleClientSecret;
+//     });
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
-});
+// builder.Services.AddDbContext<AppDbContext>(options =>
+// {
+//     options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
+// });
 
 // -- StartUp.cs - Configure
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Check DB Connection
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    try
-    {
-        dbContext.Database.OpenConnection();
-        dbContext.Database.CloseConnection();
-        Console.WriteLine("DB Connection Success");
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"DB Connection failed: {e.Message}");
-    }
-}
+// // Check DB Connection
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     try
+//     {
+//         dbContext.Database.OpenConnection();
+//         dbContext.Database.CloseConnection();
+//         Console.WriteLine("DB Connection Success");
+//     }
+//     catch (Exception e)
+//     {
+//         Console.WriteLine($"DB Connection failed: {e.Message}");
+//     }
+// }
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
