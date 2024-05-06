8bff2eebbc2278abcde89f5255767f8ec591968a
Adam-WY-Jeong <jwy0805@gmail.com>
2024-05-06 19:06:36 +0900

    db connection test

--- AccountServer/Program.cs
+++ AccountServer/Program.cs
@@ -1,6 +1,8 @@
 using System.Security.Cryptography.X509Certificates;
 using AccountServer;
+using AccountServer.DB;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 Console.WriteLine("test");
@@ -8,17 +10,17 @@ Console.WriteLine("test");
 var isLocal = Environment.GetEnvironmentVariable("ENVIRONMENT") == "Local";
 var certPath = Environment.GetEnvironmentVariable("CERT_PATH");
 var certPwd = Environment.GetEnvironmentVariable("CERT_PASSWORD");
-// if (isLocal == false)
-// {   // Kestrel Server
-//     builder.WebHost.ConfigureKestrel(options =>
-//     {
-//         options.ListenAnyIP(443, listenOptions =>
-//         {
-//             if (certPath != null && certPwd != null) listenOptions.UseHttps(certPath, certPwd);
-//             else throw new Exception("Certification path or password is null");
-//         });
-//     });
-// }
+if (isLocal == false)
+{   // Kestrel Server
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.ListenAnyIP(443, listenOptions =>
+        {
+            if (certPath != null && certPwd != null) listenOptions.UseHttps(certPath, certPwd);
+            else throw new Exception("Certification path or password is null");
+        });
+    });
+}
 
 builder.Services.AddSingleton<ConfigService>();
 
@@ -44,10 +46,10 @@ builder.Services.AddAuthentication()
         options.ClientSecret = appConfig.GoogleClientSecret;
     });
 
-// builder.Services.AddDbContext<AppDbContext>(options =>
-// {
-//     options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
-// });
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
+});
 
 // -- StartUp.cs - Configure
 Console.WriteLine("CEX");
@@ -65,21 +67,21 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// // Check DB Connection
-// using (var scope = app.Services.CreateScope())
-// {
-//     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     try
-//     {
-//         dbContext.Database.OpenConnection();
-//         dbContext.Database.CloseConnection();
-//         Console.WriteLine("DB Connection Success");
-//     }
-//     catch (Exception e)
-//     {
-//         throw new Exception($"DB Connection failed: {e.Message}");
-//     }
-// }
+// Check DB Connection
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        dbContext.Database.OpenConnection();
+        dbContext.Database.CloseConnection();
+        Console.WriteLine("DB Connection Success");
+    }
+    catch (Exception e)
+    {
+        throw new Exception($"DB Connection failed: {e.Message}");
+    }
+}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
