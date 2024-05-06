54f5aeadc51dffd0b2d96d8caaaaa50236320c59
Adam-WY-Jeong <jwy0805@gmail.com>
2024-05-06 10:05:37 +0900

    https test

--- AccountServer/Program.cs
+++ AccountServer/Program.cs
@@ -26,8 +26,7 @@ var path = Environment.GetEnvironmentVariable("CONFIG_PATH") ??
            "/Users/jwy/Documents/Dev/CryWolf/Config/CryWolfAccountConfig.json";
 var appConfig = configService.LoadGoogleConfigs(path);
 
-// Add services to the container.
-// -- StartUp.cs
+// Add services to the container. -- StartUp.cs
 var defaultConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
                               builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -44,34 +43,33 @@ builder.Services.AddAuthentication()
         options.ClientSecret = appConfig.GoogleClientSecret;
     });
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
-});
+// builder.Services.AddDbContext<AppDbContext>(options =>
+// {
+//     options.UseMySql(defaultConnectionString, new MariaDbServerVersion(new Version(10, 11, 2)));
+// });
 
 // -- StartUp.cs - Configure
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-Console.WriteLine("test1");
-// Check DB Connection
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    try
-    {
-        dbContext.Database.OpenConnection();
-        dbContext.Database.CloseConnection();
-        Console.WriteLine("DB Connection Success");
-    }
-    catch (Exception e)
-    {
-        throw new Exception($"DB Connection failed: {e.Message}");
-    }
-}
+// // Check DB Connection
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     try
+//     {
+//         dbContext.Database.OpenConnection();
+//         dbContext.Database.CloseConnection();
+//         Console.WriteLine("DB Connection Success");
+//     }
+//     catch (Exception e)
+//     {
+//         throw new Exception($"DB Connection failed: {e.Message}");
+//     }
+// }
 
-Console.WriteLine("test2");
+Console.WriteLine("test");
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
